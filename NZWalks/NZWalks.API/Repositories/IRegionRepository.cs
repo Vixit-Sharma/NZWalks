5ce7bfd5ca397126112c22c7b76029c1ffc48b26
Vixit-Sharma <vixitsharma@gmail.com>
2023-01-08 18:39:27 +0530

    End Of Section 3

---
+++ NZWalks/NZWalks.API/Repositories/IRegionRepository.cs
@@ -0,0 +1,15 @@
+﻿using NZWalks.API.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NZWalks.API.Repositories
+{
+    public interface IRegionRepository
+    {
+        //IEnumerable<Region> GetAll();
+        //Making Repository Asynchronous
+        Task<IEnumerable<Region>> GetAllAsync();
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public RegionRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            //return nZWalksDbContext.Regions.ToList();
+            return await nZWalksDbContext.Regions.ToListAsync();
+        }
+    }
+}
